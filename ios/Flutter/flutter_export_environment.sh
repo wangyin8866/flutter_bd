48c4c49e833cface0b9c1db089e49f40abe8d19a
林兴栋 <a694701@qq.com>
2019-08-16 18:56:27 +0800

    会员页

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-
 export "FLUTTER_ROOT=/Users/linxingdong/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/linxingdong/flutter_bd"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/linxingdong/flutter_bd/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/linxingdong/flutter/bin/cache/artifacts/engine/ios"
