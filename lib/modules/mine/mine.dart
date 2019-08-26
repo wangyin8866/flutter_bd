063dfb39b54e86ffe4dc0a483474173971ced153
wyman <wangyin8866@foxmail.com>
2019-08-26 17:12:52 +0800

    mine

--- lib/modules/mine/mine.dart
+++ lib/modules/mine/mine.dart
@@ -11,15 +11,27 @@ class _MinePageState extends State<MinePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
+          child: Container(
+        width: double.infinity,
+        height: double.infinity,
         child: SingleChildScrollView(
           child: Stack(
-            alignment: AlignmentDirectional.topStart,
             children: <Widget>[
               topCardWidget(),
+//            Column(
+//              children: <Widget>[
+//
+//                Container(
+//                  height: 600,
+//                  color: Color(0xffffffff),
+//                )
+//              ],
+//            ),
+              centerCardWidget()
             ],
           ),
         ),
-      ),
+      )),
     );
   }
 
@@ -74,20 +86,150 @@ class _MinePageState extends State<MinePage> {
                         SizedBox(
                           height: 3,
                         ),
-                        Text(
-                          "176 0215 614",
-                          style: TextStyle(
-                              color: Color(0xFFFFFFFF),
-                              fontSize: 14,
-                              fontWeight: FontWeight.w500),
-                        )
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              "176 0215 614",
+                              style: TextStyle(
+                                  color: Color(0xFFFFFFFF),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            SizedBox(
+                              width: 3,
+                            ),
+                            Text(
+                              ">",
+                              style: TextStyle(
+                                  color: Color(0xFFFFFFFF),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500),
+                            )
+                          ],
+                        ),
                       ],
-                    ))
+                    )),
+                Expanded(
+                    flex: 1,
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(flex: 1, child: SizedBox()),
+                        Container(
+                          width: 102,
+                          height: 36,
+                          decoration: BoxDecoration(
+                              color: Color(0xFFFFFFFF),
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(44),
+                                  topRight: Radius.circular(0),
+                                  bottomRight: Radius.circular(0),
+                                  bottomLeft: Radius.circular(44))),
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(10, 5, 5, 5),
+                            child: Row(
+                              children: <Widget>[
+                                Image.asset(
+                                  "images/but_weixin_login.png",
+                                  fit: BoxFit.contain,
+                                  width: 20,
+                                  height: 20,
+                                ),
+                                SizedBox(
+                                  width: 7,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      "奖励金",
+                                      style: TextStyle(
+                                          color: Color(0xFFD61A1A),
+                                          fontSize: 11,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    Text(
+                                      "¥500.00",
+                                      style: TextStyle(
+                                          color: Color(0xFFD61A1A),
+                                          fontSize: 11,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                  ],
+                                ),
+                                Expanded(
+                                    flex: 1,
+                                    child: Stack(
+                                      alignment: Alignment.centerRight,
+                                      children: <Widget>[
+                                        Positioned(
+                                            child: Text(
+                                          ">",
+                                          style: TextStyle(
+                                              color: Color(0xFFD61A1A),
+                                              fontSize: 11,
+                                              fontWeight: FontWeight.w500),
+                                        ))
+                                      ],
+                                    ))
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    )),
               ],
             ),
           )
         ],
       ),
     );
+  }
+
+  Widget centerCardWidget() {
+    return Positioned(
+        top: 145,
+        right: 18,
+        left: 20,
+        child: Container(
+          decoration: BoxDecoration(
+              color: Color(0xFFFFFFFF),
+              borderRadius: BorderRadius.circular(6),
+              boxShadow: [
+                BoxShadow(
+                    color: Color(0xfff1eeee),
+                    offset: Offset(0, 8),
+                    blurRadius: 9)
+              ]),
+          width: 338,
+          height: 213,
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Text(
+                      "个人今日数据",
+                      style: TextStyle(
+                          color: Color(0xFFD61A1A),
+                          fontSize: 13,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Text(
+                      "个人今日数据",
+                      style: TextStyle(
+                          color: Color(0xFFD61A1A),
+                          fontSize: 13,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
